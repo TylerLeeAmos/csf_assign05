f4928381bda38c512a487c69831f5b6ae67a0b8b
Tyler Amos <tamos5@jhu.edu>
2020-11-17 11:57:07 -0500

    initial setup

---
+++ calc.cpp
@@ -0,0 +1,47 @@
+#include "calc.h"
+#include <vector>
+#include <string>
+#include <sstream>
+using std::stringstream;
+using std::vector;
+using std::string;
+
+
+struct Calc {
+};
+
+class CalcImpl : public Calc {
+
+
+
+  return 0;
+
+};
+
+
+extern "C" struct Calc *calc_create(void) {
+  return new CalcImpl();
+}
+
+extern "C" void calc_destroy(struct Calc *calc) {
+  CalcImpl *obj = static_cast<CalcImpl *>(calc);
+  delete obj;
+}
+
+extern "C" int calc_eval(struct Calc *calc, const char *expr, int *result) {
+  CalcImpl *obj = static_cast<CalcImpl *>(calc);
+  return obj->evalExpr(expr, *result);
+}
+
+vector<string> tokenize(const string &expr) {
+  vector<string> vec;
+  stringstream s(expr);
+
+  string tok;
+  while (s >> tok) {
+    vec.push_back(tok);
+  }
+
+  return vec;
+}
+  
